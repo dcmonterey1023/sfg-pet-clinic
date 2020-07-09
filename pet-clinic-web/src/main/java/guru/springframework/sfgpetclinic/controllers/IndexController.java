eb5464fa60a3d26ff1959b7c1a3967cc82f16710
dcmonterey1023 <d.monterey@samsung.com>
2020-07-09 12:32:28 +0800

    Adding index page and controller. Closes #8

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author d.monterey on 09/07/2020
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+
+        return "index";
+    }
+
+}
