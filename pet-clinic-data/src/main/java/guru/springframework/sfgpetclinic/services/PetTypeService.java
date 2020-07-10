a70d2f1e31708cba9c27eba8e53544caaa996ea9
dcmonterey1023 <d.monterey@samsung.com>
2020-07-10 17:39:53 +0800

    enhance owners and pet and contacts

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * @author d.monterey on 10/07/2020
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+
+
+}
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,11 +1,16 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 /**
  * @author d.monterey on 09/07/2020
@@ -15,24 +20,55 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType saveDogPetType = petTypeService.save(dog);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Juan");
         owner1.setLastName("Cruz");
+        owner1.setAddress("Magungunay");
+        owner1.setCity("La Union");
+        owner1.setTelephone("123422121");
+
+        Pet juanPet = new Pet();
+        juanPet.setPetType(saveDogPetType);
+        juanPet.setOwner(owner1);
+        juanPet.setBirthDate(LocalDate.now());
+        juanPet.setName("Iron Man");
+        owner1.getPets().add(juanPet);
 
         ownerService.save(owner1);
+
 
         Owner owner2 = new Owner();
         owner2.setFirstName("John");
         owner2.setLastName("Doe");
+        owner2.setAddress("Magungunay");
+        owner2.setCity("La Union");
+        owner2.setTelephone("123422121");
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType saveCatPetType = petTypeService.save(cat);
+
+        Pet johnPet = new Pet();
+        johnPet.setPetType(saveCatPetType);
+        johnPet.setOwner(owner2);
+        johnPet.setBirthDate(LocalDate.now());
+        johnPet.setName("Captain America");
+        owner2.getPets().add(johnPet);
 
         ownerService.save(owner2);
 
